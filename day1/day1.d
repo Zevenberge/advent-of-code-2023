27dae9ad4e2391cd672e0f36032f86763c6a798e
Zevenberge <nanayamae@gmail.com>
2023-12-01 07:17:07 +0100

    day1b

--- day1/day1.d
+++ day1/day1.d
@@ -1,5 +1,35 @@
 import std;
 
+
+dchar[] toNumbers(string input)
+{
+    const dictionary = ["one": 1, "two": 2, "three": 3, "four": 4, 
+        "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9];
+    dchar[] output;
+    foreach (i, c; input)
+    {
+        if(isNumber(c))
+        {
+            output ~= c;
+        }
+        else
+        {
+            foreach (width; 3..6)
+            {
+                const boundary = min(i + width, input.length);
+                const key = input[i .. boundary];
+                const number = key in dictionary;
+                if(number)
+                {
+                    output ~= (*number).to!dstring;
+                    break;
+                }
+            }
+        }
+    }
+    return output;
+}
+
 bool isNumber(dchar character)
 {
     char c = character.to!char;
@@ -10,7 +40,7 @@ void main()
 {
     File("input").byLineCopy()
         .filter!(line => line.length > 0)
-        .map!(line => line.filter!(c => isNumber(c)).array)
+        .map!(line => line.toNumbers)
         .map!(numbers => [numbers.front, numbers.back])
         .map!(number => number.to!int)
         .sum
