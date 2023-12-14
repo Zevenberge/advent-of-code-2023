72e6214337dc67b94763f4980665be2bb458ba80
Zevenberge <nanayamae@gmail.com>
2023-12-14 21:06:43 +0100

    iterating on a solution

--- day14/day14.d
+++ day14/day14.d
@@ -36,43 +36,89 @@ class Boulder
 
 enum emptySpace = '.';
 
-Boulder[][] columns(string[] lines)
+Boulder[] boulders(string[] lines)
 {
-    auto boulders = new Boulder[][lines.length];
+    Boulder[] boulders;
     foreach(y, line; lines)
     {
         foreach(x, c; line)
         {
             if(c != emptySpace)
             {
-                boulders[x] ~= new Boulder(c, Coordinate(x, y));
+                boulders ~= new Boulder(c, Coordinate(x, y));
             }
         }
     }
     return boulders;
 }
 
-Boulder[] moveUp(Boulder[] column)
+alias byColumn = orderBy!("x", (a, b) => a.coordinate.y < b.coordinate.y);
+alias byColumnDesc = orderBy!("x", (a, b) => a.coordinate.y > b.coordinate.y);
+alias byRow = orderBy!("y", (a, b) => a.coordinate.x < b.coordinate.x);
+alias byRowDesc = orderBy!("y", (a, b) => a.coordinate.x > b.coordinate.x);
+
+Boulder[][] orderBy(string property, alias pred)(Boulder[] boulders)
+{
+    enum fullProperty = "coordinate." ~ property;
+    return boulders.sort!("a." ~ fullProperty ~ " < b." ~ fullProperty)
+        .chunkBy!(b => __traits(getMember, b.coordinate, property))
+        .map!(g => g[1].array.sort!pred.array).array;
+}
+
+enum Direction { ascending, descending }
+
+alias moveUp = move!("y", Direction.descending);
+alias moveDown = move!("y", Direction.ascending);
+alias moveLeft = move!("x", Direction.descending);
+alias moveRight = move!("x", Direction.ascending);
+
+Boulder[] move(string property, Direction direction)(Boulder[] series)
 {
     size_t top;
-    foreach(boulder; column)
+    foreach(boulder; series)
     {
         if(boulder.isStatic)
         {
-            top = boulder.coordinate.y;
+            top = __traits(getMember, boulder.coordinate, property);
         }
         else
         {
-            boulder.moveToY(top);
+            __traits(getMember, boulder.coordinate, property) = top;
         }
-        top += 1;
+        top += direction == Direction.descending ? 1 : -1;
     }
-    return column;
+    return series;
+}
+
+alias Set = bool[Coordinate];
+
+Boulder[] rotate(Boulder[] boulders)
+{
+    boulders.byColumn.each!moveUp;
+    boulders.byRow.each!moveLeft;
+    boulders.byColumnDesc.each!moveDown;
+    boulders.byRowDesc.each!moveRight;
+    return boulders;
+}
+
+size_t state(Boulder[] boulders)
+{
+    return boulders.filter!(b => !b.isStatic).map!(b => b.coordinate.x * 101 + b.coordinate.y).fold!((a, b) => a*b);
 }
 
 void main()
 {
     auto lines = File("input").byLineCopy().filter!(line => line.length > 0).array;
-    lines.columns.map!(c => c.moveUp.map!(b => b.weight(lines.length))).joiner.sum.writeln;
+    auto gridSize = lines.length;
+    auto boulders = lines.boulders;
+    bool[size_t] previousState;
+    for(size_t i = 0; i < 1_000_000_000 ; i++)
+    {
+        boulders.rotate;
+        auto newState = boulders.state;
+        if(newState in previousState) break;
+        else previousState[newState] = true;
+    }
+    boulders.map!(b => b.weight(gridSize)).sum.writeln;
 }
 
